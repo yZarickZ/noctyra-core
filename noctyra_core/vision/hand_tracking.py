be38cb8f920402cf12e904dfb418ed252b46863c
yZarickZ <viniciusmarsan22@gmail.com>
2026-05-17 20:15:35 -0300

    feat: implement initial hand tracking system

---
+++ noctyra_core/vision/hand_tracking.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import mediapipe as mp
+
+class HandTracker:
+    def __init__(self):
+        self.mp_hands = mp.solutions.hands
+        self.mp_draw = mp.solutions.drawing_utils
+        self.hands = self.mp_hands.Hands(
+            static_image_mode=False,
+            max_num_hands=2,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+
+    def process(self, frame):
+        frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        results = self.hands.process(frame_rgb)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mp_draw.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    self.mp_hands.HAND_CONNECTIONS
+                )
+
+        return frame, results
